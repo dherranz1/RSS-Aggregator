1e54016422a481fc779168015814e6e3c6786b39
dherranz1 <dweshh@proton.me>
2022-11-19 11:35:07 +0100

    Issue 10, added new feature get rss sources in data layer

--- app/src/main/java/com/dherranz1/rss_aggregator/data/local/LocalDataSource.kt
+++ app/src/main/java/com/dherranz1/rss_aggregator/data/local/LocalDataSource.kt
@@ -1,5 +1,8 @@
 package com.dherranz1.rss_aggregator.data.local
 
+import com.dherranz1.rss_aggregator.domain.SourceRss
+
 interface LocalDataSource {
     fun save(name : String, url : String)
+    fun getAllSources() : List<SourceRss>
 }
