580a3f9d50bd0d910ba6d0579008b61181531ef1
dherranz1 <dweshh@proton.me>
2022-12-09 22:11:47 +0100

    navigation and bottom navigation added

--- app/src/main/java/com/dherranz1/rss_aggregator/MainActivity.kt
+++ app/src/main/java/com/dherranz1/rss_aggregator/MainActivity.kt
@@ -2,10 +2,29 @@ package com.dherranz1.rss_aggregator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpBottomNavigation()
+    }
+
+    private fun setUpBottomNavigation(){
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+
+        bottomNav.setOnItemSelectedListener { menuItem ->
+            val navController = findNavController(R.id.nav_host_fragment)
+
+            when(menuItem.itemId){
+                R.id.bottom_nav_menu_item_rss -> navController.navigate(R.id.rssFeedFragment)
+                R.id.bottom_nav_menu_item_management -> navController.navigate(R.id.rssManagementFragment)
+                R.id.bottom_nav_menu_item_profile -> navController.navigate(R.id.profileFragment)
+            }
+            true
+        }
     }
 }
