8239ce863b16b30d3224a196a0b9bfa4bc61d952
dherranz1 <dweshh@proton.me>
2022-12-12 01:12:22 +0100

    bottom sheet fixed behavior

--- app/src/main/java/com/dherranz1/rss_aggregator/presentation/RssManagementFragment.kt
+++ app/src/main/java/com/dherranz1/rss_aggregator/presentation/RssManagementFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.fragment.app.DialogFragment
 import com.dherranz1.rss_aggregator.R
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class RssManagementFragment : Fragment() {
 
@@ -26,7 +28,11 @@ class RssManagementFragment : Fragment() {
 
         val addButton = view.findViewById<ImageView>(R.id.toolbar_manage_button_add)
 
+        val modalBottomSheetBehavior =
+        // Use this to programmatically apply behavior attributes
+
         addButton.setOnClickListener {
+            modalBottomSheet.setStyle(DialogFragment.STYLE_NORMAL,R.style.DialogStyle)
             modalBottomSheet.show(childFragmentManager, BottomSheetDialog.TAG)
         }
 
