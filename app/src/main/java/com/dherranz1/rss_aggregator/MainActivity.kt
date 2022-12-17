56045799776b21a836bf064ddf6d67fcafbe46b0
dherranz1 <dweshh@proton.me>
2022-12-17 18:23:35 +0100

    Navigation with navgraph directions

--- app/src/main/java/com/dherranz1/rss_aggregator/MainActivity.kt
+++ app/src/main/java/com/dherranz1/rss_aggregator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.dherranz1.rss_aggregator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
+import com.dherranz1.rss_aggregator.presentation.RssFeedFragmentDirections
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -20,9 +21,9 @@ class MainActivity : AppCompatActivity() {
             val navController = findNavController(R.id.nav_host_fragment)
 
             when(menuItem.itemId){
-                R.id.bottom_nav_menu_item_rss -> navController.navigate(R.id.rssFeedFragment)
-                R.id.bottom_nav_menu_item_management -> navController.navigate(R.id.rssManagementFragment)
-                R.id.bottom_nav_menu_item_profile -> navController.navigate(R.id.profileFragment)
+                R.id.bottom_nav_menu_item_rss -> navController.navigate(NavGraphDirections.actionToRssFeedFragment())
+                R.id.bottom_nav_menu_item_management -> navController.navigate(NavGraphDirections.actionToRssManagementFragment())
+                R.id.bottom_nav_menu_item_profile -> navController.navigate(NavGraphDirections.actionToProfileFragment())
             }
             true
         }
